c744880b01bcc23639ce13affac7f69a08d860ac
adsupe <adarsh.supe@capgemini.com>
2024-07-25 18:18:41 +0530

    Update by master SOP AddCommaToElementExample.java

--- TestUltra/src/ultimate/java/progs/AddCommaToElementExample.java
+++ TestUltra/src/ultimate/java/progs/AddCommaToElementExample.java
@@ -10,7 +10,7 @@ public class AddCommaToElementExample {
 		List<Integer> numList = Arrays.asList(2,3,4,5,6,7,8);
 		List<String> strList = numList.stream().map(i->String.valueOf(i)).collect(Collectors.toList());
 		String result = String.join(", ",numList.stream().parallel().map(i->String.valueOf(i)).collect(Collectors.toList()));
-		System.out.println(result);
+		System.out.println("Add comma :: "+result);
 
 	}
 
